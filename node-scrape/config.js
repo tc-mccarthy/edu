df5e1f7b7d2cc39d6b326fd7c343294a0cb24e3e
TC McCarthy <TC.McCarthy1@gmail.com>
2015-09-17 19:16:02 -0400

    Adds more templates

--- node-scrape/config.js
+++ node-scrape/config.js
@@ -1,16 +1,9 @@
 exports.config = {
 	mysql: {
-		user: process.env.MYSQL_USER,
-		pass: process.env.MYSQL_PASS,
-		db: "SCHEMA NAME",
-		host: process.env.MYSQL_HOST,
+		user: "root",
+		pass: "root",
+		db: "jrnscrape",
+		host: "localhost",
 		port: 3306
-	},
-
-	app: {
-		port: 9000,
-		//home: "/admin/valedictorians/",
-		home: "/",
-		logging: true
 	}
 };
