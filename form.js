cf6036372844130322f2e83e78921ef2eb2f5052
TC McCarthy <tc.mccarthy1@gmail.com>
2017-02-28 18:30:11 -0500

    Adds form calculation example

--- form.js
+++ form.js
@@ -6,9 +6,26 @@ $(document).ready(function () {
 		var b = parseFloat($("#b").val());
 		var c = parseFloat($("#c").val());
 
+		var poop = quadSolve(a, b, c);
 
-		var answer = ((b * b) - (4 * a * c)) / (2 * a);
+		$("#answer").val(poop);
+	});
 
-		$("#answer").val(answer);
+	$("#button2").on("click", function (e) {
+		e.preventDefault();
+
+		var d = parseFloat($("#d").val());
+		var e = parseFloat($("#e").val());
+		var f = parseFloat($("#f").val());
+
+		var poop = quadSolve(d, e, f);
+
+		$("#answer2").val(poop);
 	});
 });
+
+function quadSolve(a, b, c) {
+	var poop = ((b * b) - (4 * a * c)) / (2 * a);
+
+	return poop;
+}
