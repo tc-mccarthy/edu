9a39e0850c1aedc20897e49b4ed555c821e96901
TC McCarthy <tc.mccarthy1@gmail.com>
2017-11-02 19:07:07 -0400

    Updates JS

--- exercises/js/youtube-scroll-spy/youtube.js
+++ exercises/js/youtube-scroll-spy/youtube.js
@@ -1,3 +1,5 @@
+var videos = [];
+var apiReady = false;
 var CUNY_YT = function (ele) {
 	var self = this,
 		width = ele.width(),
@@ -19,9 +21,14 @@ var CUNY_YT = function (ele) {
 	ele.wrap(wrapper);
 	ele.attr("id", player_id);
 
+	var container = ele.closest(".video");
+
+	this.ready = false;
+
 	var onPlayerReady = function (event) {
 		console.log("The player is ready");
 		//player ready stuff
+		self.ready = true;
 	};
 
 	var onPlayerStateChange = function (event) {
@@ -44,17 +51,60 @@ var CUNY_YT = function (ele) {
 	//abstraction methods in case the YouTube API ever changes
 	this.stop = function () {
 		self.player.stopVideo();
-	}
+	};
 
 	this.play = function () {
 		self.player.playVideo();
-	}
+	};
 
+	this.pause = function () {
+		self.player.pauseVideo();
+	};
+
+	this.mute = function () {
+		self.player.mute();
+	};
+
+	this.unMute = function () {
+		self.player.unMute();
+	};
+
+	$(window).on("scroll", function () {
+		console.log("apiReady", apiReady);
+		console.log("self.ready", self.ready);
+		if (apiReady === true && self.ready === true) {
+			console.log("Scrolling");
+			if (isInView(container)) {
+				console.log("playing video");
+				self.mute();
+				self.play();
+			} else {
+				console.log("Pausing video");
+				self.pause();
+			}
+		}
+	});
 };
 
-var videos = [];
+var isInView = function (el) {
+	if (typeof el == "undefined") {
+		return false;
+	}
+
+	if (typeof el === "object") {
+		el = el[0];
+	}
+
+	var rect = el.getBoundingClientRect();
+
+	return rect.bottom > 0 &&
+		rect.right > 0 &&
+		rect.left < (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */ &&
+		rect.top < (window.innerHeight || document.documentElement.clientHeight) /*or $(window).height() */ ;
+};
 
 function onYouTubeIframeAPIReady() {
+	apiReady = true;
 	$(".youtube").each(function () {
 		videos.push(new CUNY_YT($(this)));
 	});
