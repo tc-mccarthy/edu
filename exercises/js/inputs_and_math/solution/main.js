bd23ab9bff26bff0f7b1ae64e6391f15b4d5813e
TC McCarthy <tc.mccarthy1@gmail.com>
2019-03-19 17:06:37 -0400

    Updates the solution

--- exercises/js/inputs_and_math/solution/main.js
+++ exercises/js/inputs_and_math/solution/main.js
@@ -1,33 +1,67 @@
 $("body").on("keyup keydown keypress change", "input", function (e) {
-	var this_input = $(this),
-		this_row = this_input.closest(".item"),
-		quantity = parseFloat(this_row.find(".qty").val()),
-		cost = parseFloat(this_row.find(".cost").val()),
-		tariff = parseFloat(this_row.data("tariff-percent")),
-		pre_tariff_cost = (cost * quantity),
-		post_tariff_cost = (pre_tariff_cost + (pre_tariff_cost * (tariff / 100)));
+	const $this_input = $(this);
+	const $this_row = $this_input.closest("[data-tariff-percent]");
+	const $span_pre_total = $this_row.find(".pre_total").find("span");
+	const $span_post_total = $this_row.find(".post_total").find("span");
+	let qty = $this_row.find(".qty").find("input").val();
+	let cost = $this_row.find(".cost").find("input").val();
+	let tariff = $this_row.data("tariff-percent");
 
-	// console.log("Quantity", quantity);
+	if (qty === "" || cost === "") {
+		return;
+	}
 
-	this_row.find(".pre_total").find("span").text(pre_tariff_cost);
-	this_row.find(".post_total").find("span").text(post_tariff_cost);
+	qty = parseFloat(qty);
+	cost = parseFloat(cost);
+	tariff = parseFloat(tariff);
 
-	var pre_total = 0,
+	let tariff_decimal = tariff / 100;
+	let pre_tariff = qty * cost;
+	let post_tariff = (qty * cost) * (1 + tariff_decimal);
+
+	console.log("Qty", qty);
+	console.log("Cost", cost);
+	console.log("Tariff", tariff);
+
+	console.log("pre_tariff", pre_tariff);
+	console.log("post_tariff", post_tariff);
+
+	$span_pre_total.text(pre_tariff);
+	$span_post_total.text(post_tariff);
+
+	let pre_total = 0,
 		post_total = 0;
 
 	$(".pre_total").each(function () {
-		//do stuff
-		var this_pre_total = parseFloat($(this).find("span").text());
-		pre_total = pre_total + this_pre_total;
+		const $this_pre_total = $(this);
+		const $this_pre_total_span = $this_pre_total.find("span");
+
+		let value = $this_pre_total_span.text();
+
+		if (value == "") {
+			return;
+		}
+
+		value = parseFloat(value);
+
+		pre_total = pre_total + value;
 	});
 
 	$(".post_total").each(function () {
-		//more stuff
-		var this_post_total = parseFloat($(this).find("span").text());
-		post_total = post_total + this_post_total;
+		const $this_post_total = $(this);
+		const $this_post_total_span = $this_post_total.find("span");
+
+		let value = $this_post_total_span.text();
+
+		if (value == "") {
+			return;
+		}
+
+		value = parseFloat(value);
+
+		post_total = post_total + value;
 	});
 
 	$("#pre_total").find("span").text(pre_total);
 	$("#post_total").find("span").text(post_total);
-
 });
