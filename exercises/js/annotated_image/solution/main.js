8f745fd55e9bd5567844d1bc4faee103b541fc86
TC McCarthy <tc.mccarthy@newsday.com>
2021-03-06 21:45:25 -0500

    Cleans up solution JS

--- exercises/js/annotated_image/solution/main.js
+++ exercises/js/annotated_image/solution/main.js
@@ -6,19 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
         dot.addEventListener("click", ev => {
             ev.preventDefault();
             const clicked_dot = ev.target.closest(".dot");
-            clicked_dot.classList.add("active");
-        });
-    });
-
-    close_buttons.forEach(close_button => {
-        close_button.addEventListener("click", e => {
-            const clicked_dot = e.target.closest(".dot");
 
             if (clicked_dot.classList.contains("active")) {
-                e.preventDefault();
-                e.stopPropagation();
-
                 clicked_dot.classList.remove("active");
+            } else {
+                clicked_dot.classList.add("active");
             }
         });
     });
